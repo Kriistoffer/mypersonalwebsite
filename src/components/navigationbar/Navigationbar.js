e0228e24d81fbbb36224a3d242cfce8079e30d58
Kristoffer <kjohansson93@hotmail.se>
2021-11-29 18:33:35 +0100

    Started off with the navbar

---
+++ src/components/navigationbar/Navigationbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigationbar = () => {
+    return (
+        <div className="navigation-menu">
+            
+        </div>
+    )
+}
+
+export default Navigationbar;
