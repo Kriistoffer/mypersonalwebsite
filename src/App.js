8d4ee7f4370e8c825fa9ee66fed23f683d6a5d9e
Kristoffer <kjohansson93@hotmail.se>
2021-12-15 23:52:11 +0100

    Added basic structure of the first pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Navigationbar from './components/navigationbar/Navigationbar';
+import Homepage from './pages/Homepage/Homepage';
+import About from './pages/About/About';
+import Projects from './pages/Projects/Projects';
 
 function App() {
   return (
     <div className="App">
       <Navigationbar />
+      <Homepage />
+      <About />
+      <Projects />
     </div>
   );
 }
